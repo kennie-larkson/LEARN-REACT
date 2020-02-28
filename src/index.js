30ee46ea7e3216dab6644665c1fa2f48c34e7239
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-28 13:06:19 +0100

    added more HTML nodes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myFirstElement = <h1>Hello React!</h1> 
+const myFirstElement = (<h1>Hello React!</h1> );
 
+const myElement = (
+    <table >
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>Lawal</td>
+        </tr>
+        <tr>
+            <td>Abdulrafiu</td>
+        </tr>
+    </table>
+);
 
 ReactDOM.render(myFirstElement, document.getElementById('root'));
+ReactDOM.render(<p>Hello</p>, document.getElementById('root2'));
+ReactDOM.render(myElement, document.getElementById('root3'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
