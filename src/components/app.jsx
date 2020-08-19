05ffb09bc804aafc7b7d5575f73d789687406d57
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-08-19 16:43:23 +0100

    layout extended

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./form";
 import Paragraph from "./paragraph";
 
 function App() {
+  const [name, setName] = useState("Kennie");
+
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at enim sit amet lorem volutpat luctus pretium lacinia purus. Fusce hendrerit, leo sit amet faucibus congue, lectus ipsum cursus enim, hendrerit porttitor risus nunc vitae risus. Cras consectetur ipsum felis, non molestie ante aliquam a. In eleifend pulvinar massa, vitae consequat risus cursus tincidunt. Morbi semper mauris ut sapien fringilla faucibus. Morbi nec laoreet diam. In at ultrices eros, sed auctor risus.";
   return (
     <div className="app_component">
-      <Paragraph
-        className="paragraph_one row_one"
-        text="This is the first paragraph in the App component view. And it is on the first row and first column of the grid"
-      />
+      <div className="row_one">
+        <Paragraph className="col_one_row_one" text={text} />
 
-      <Form />
+        <Form className="col_two_row_one" />
+        <Paragraph className="col_three_row_one" text={text} />
+      </div>
+      <div className="row_two">
+        <Paragraph className="col_one_row_two" text={text} />
+      </div>
+      <div className="row_three">
+        <Paragraph className="col_one_row_two" text={text} />
+      </div>
     </div>
   );
 }
